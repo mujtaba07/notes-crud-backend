899e6a8dc3cc7b184e1121da7bde0f3f11647989
Mujtaba <mustafasayyed46@gmail.com>
2026-04-10 04:07:45 +0530

    backend notes crud operation

---
+++ src/app.js
@@ -0,0 +1,40 @@
+//create the server and export it for use in server.js
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
+  res.status(200).json({
+    message: "Note added successfully",
+  });
+});
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    notes: notes,
+  });
+});
+
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  delete notes[index] ;
+  res.status(200).json({
+    message: "Note deleted successfully"
+  });
+});
+
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const description = req.body.description;
+  notes[index].description = description;
+  res.status(200).json({
+    message: "Note updated successfully"
+  });
+});
+
+module.exports = app;
